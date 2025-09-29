56472edf81d7bd97c62eaebe9a6078c88a370e50
Noa Hallklint <95153555+blockplocker@users.noreply.github.com>
2025-09-29 11:38:13 +0200

    Search page (#3)
    * feat: update application configuration and routing; add environment files for development
    
    * feat: enhance search functionality with loading state and error handling; update search UI

--- PetRegistry/src/app/app.config.ts
+++ PetRegistry/src/app/app.config.ts
@@ -1,12 +1,22 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { API_BASE_URL_PETREGISTRY, Client } from './domain/client';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideHttpClient(),
+    Client,
+    { provide: API_BASE_URL_PETREGISTRY, useValue: environment.API_URL },
+  ],
 };
---
+++ PetRegistry/src/environments/environment.ts
@@ -0,0 +1,5 @@
+//For API url that is in live use
+
+export const environment = {
+  API_URL: 'https://localhost:44391',
+};
