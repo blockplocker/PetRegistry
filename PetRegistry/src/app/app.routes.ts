522a0825c1bd3a36443706ad2436ee9f2049164f
blockplocker <noahallklint@gmail.com>
2025-09-29 09:15:56 +0200

    feat: add main application structure with routing and components
    - Implemented routing for Home, Persons, and Pets components.
    - Created Header component with navigation links.
    - Developed Home, Persons, Pets, and Search components with basic templates.
    - Added unit tests for all new components.
    - Included CSS files for styling components.

--- PetRegistry/src/app/app.routes.ts
+++ PetRegistry/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Persons } from './persons/persons';
+import { Pets } from './pets/pets';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: '',
+    component: Home,
+    // canActivate: [AuthGuard],
+    title: 'Home - PetRegistry',
+    },
+    {
+    path: 'persons',
+    component: Persons,
+    title: 'Persons - PetRegistry',
+  },
+    {
+    path: 'pets',
+    component: Pets,
+    title: 'Pets - PetRegistry',
+  },
+];
---
+++ PetRegistry/src/app/persons/persons.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-persons',
+  imports: [],
+  templateUrl: './persons.html',
+  styleUrl: './persons.css'
+})
+export class Persons {
+
+}
