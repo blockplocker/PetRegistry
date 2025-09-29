f75f0bf273639b6f6a2602117e1c59235ce76234
Teddy . <Teddy_95@live.se>
2025-09-29 15:43:18 +0200

    added saveperson func to person

--- PetRegistry/src/app/persons/persons.ts
+++ PetRegistry/src/app/persons/persons.ts
@@ -1,11 +1,54 @@
 import { Component } from '@angular/core';
+import { PersonService } from '../Services/person-service';
+import { PersonDto } from '../domain/client';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Dialog } from '@angular/cdk/dialog';
+import { AppDialogComponent } from '../components/Dialog/Dialog';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DialogModule } from '@angular/cdk/dialog';
 
 @Component({
   selector: 'app-persons',
-  imports: [],
+  imports: [ReactiveFormsModule, DialogModule],
   templateUrl: './persons.html',
-  styleUrl: './persons.css'
+  standalone: true,
+  styleUrls: ['./persons.css'],
 })
 export class Persons {
+  personForm!: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    private personService: PersonService,
+    private dialog: Dialog
+  ) {
+    this.personForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      address: ['', Validators.required],
+      city: ['', Validators.required],
+      phoneNumber: ['', [Validators.required, Validators.pattern(/^[0-9]{8,15}$/)]],
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
 
+  save() {
+    if (this.personForm.valid) {
+      const dto = this.personForm.value as PersonDto;
+
+      this.personService.savePerson(dto).subscribe({
+        next: (res) => {
+          this.dialog.open(AppDialogComponent, {
+            data: { title: 'Sparat!', message: 'Personen har sparats!' },
+          });
+
+          this.personForm.reset();
+        },
+        error: (err) => {
+          this.dialog.open(AppDialogComponent, {
+            data: { title: 'Fel vid sparning', message: 'Kunde inte spara person' },
+          });
+        },
+      });
+    }
+  }
 }
