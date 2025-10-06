9eafba0176a9088090cb3e4589bf4d462cb79972
Teddy . <Teddy_95@live.se>
2025-10-06 12:57:56 +0200

    added maxLenght to  pet and person form and  petdto/persondto in api

--- PetRegistryAPI/PetRegistryAPI/Dto/PersonDto.cs
+++ PetRegistryAPI/PetRegistryAPI/Dto/PersonDto.cs
@@ -7,21 +7,27 @@ namespace PetRegistryAPI.Dto
         public int Id { get; set; }
 
         [Required]
+        [MaxLength(15)] 
         public string FirstName { get; set; }
 
         [Required]
+        [MaxLength(20)] 
         public string LastName { get; set; }
 
         [Required]
+        [MaxLength(25)] 
         public string Address { get; set; }
 
         [Required]
+        [MaxLength(25)] 
         public string City { get; set; }
 
         [Required]
+        [MaxLength(15)] 
         public string PhoneNumber { get; set; }
 
         [Required]
+        [MaxLength(50)] 
         public string Email { get; set; }
     }
 }
--- PetRegistryAPI/PetRegistryAPI/Dto/PetDto.cs
+++ PetRegistryAPI/PetRegistryAPI/Dto/PetDto.cs
@@ -9,16 +9,20 @@ namespace PetRegistryAPI.Dto
         public int Id { get; set; }
 
         [Required]
+        [MaxLength(15)] 
         public string Name { get; set; }
 
         [Required]
         public string Gender { get; set; }
 
         [Required]
+        [MaxLength(15)] 
         public string Species { get; set; }
 
+        [MaxLength(15)] 
         public string? Breed { get; set; }
         public string? DateOfBirth { get; set; }
+        [MaxLength(15)] 
         public string? Color { get; set; }
         public bool? IsMicrochip { get; set; }
         public bool? IsNeutered { get; set; }
