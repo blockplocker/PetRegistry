43c31d05044683285a566a02510251d4959a27cd
blockplocker <noahallklint@gmail.com>
2025-10-07 13:46:38 +0200

    enhance input validation messages and string sanitization methods

--- PetRegistry/src/app/Services/Utils/string-utils.ts
+++ PetRegistry/src/app/Services/Utils/string-utils.ts
@@ -2,4 +2,11 @@ export class StringUtils {
   static capitalizeFirst(value: string) {
     return value ? value[0].toUpperCase() + value.slice(1).toLowerCase() : value;
   }
+  static sanitizeInput(value: string) {
+    if (!value) return value;
+    value = value.replace(/<[^>]*>/gm, ''); // Remove HTML tags
+    value = value.replace(/[<>'"&]/g, ''); // Remove special characters
+    value = value.trim(); // Trim whitespace
+    return value;
+  }
 }
