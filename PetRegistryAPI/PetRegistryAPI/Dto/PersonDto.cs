9b85e14cc7af81e84ed301b6df11c17b256a8a91
Teddy . <Teddy_95@live.se>
2025-10-07 10:45:28 +0200

    Added maximum length attributes with custom error messages for validation. Introduced regular expression validation for PhoneNumber and EmailAddress attribute for Email in PersonDto. Updated Breed and Color properties in PetDto with maximum length attributes and custom error messages.

--- PetRegistryAPI/PetRegistryAPI/Dto/PersonDto.cs
+++ PetRegistryAPI/PetRegistryAPI/Dto/PersonDto.cs
@@ -7,27 +7,28 @@ namespace PetRegistryAPI.Dto
         public int Id { get; set; }
 
         [Required]
-        [MaxLength(15)] 
-        public string? FirstName { get; set; }
+        [MaxLength(15, ErrorMessage = "Firstname has a max lenght of 15")] 
+        public string FirstName { get; set; }
 
         [Required]
-        [MaxLength(20)] 
-        public string? LastName { get; set; }
+        [MaxLength(20, ErrorMessage = "LastName has a max lenght of 20")] 
+        public string LastName { get; set; }
 
         [Required]
-        [MaxLength(25)] 
-        public string? Address { get; set; }
+        [MaxLength(25, ErrorMessage = "Address has a max lenght of 25")] 
+        public string Address { get; set; }
 
         [Required]
-        [MaxLength(25)] 
-        public string? City { get; set; }
+        [MaxLength(25, ErrorMessage = "City has a max lenght of 25")] 
+        public string City { get; set; }
 
         [Required]
-        [MaxLength(15)] 
-        public string? PhoneNumber { get; set; }
+        [RegularExpression(@"^[0-9]{10,15}$", ErrorMessage = "Invalid phone number format")]
+        public string PhoneNumber { get; set; }
 
         [Required]
-        [MaxLength(50)] 
-        public string? Email { get; set; }
+        [MaxLength(50)]
+        [EmailAddress]
+        public string Email { get; set; }
     }
 }
--- PetRegistryAPI/PetRegistryAPI/Dto/PetDto.cs
+++ PetRegistryAPI/PetRegistryAPI/Dto/PetDto.cs
@@ -9,21 +9,21 @@ namespace PetRegistryAPI.Dto
         public int Id { get; set; }
 
         [Required]
-        [MaxLength(15)] 
-        public string? Name { get; set; } 
+        [MaxLength(15, ErrorMessage = "Name has a max lenght of 15")] 
+        public string Name { get; set; } 
 
         [Required]
-        [MaxLength(10)]
-        public string? Gender { get; set; }
+        [MaxLength(10, ErrorMessage = "Gender has a max lenght of 10")]
+        public string Gender { get; set; }
 
         [Required]
-        [MaxLength(15)] 
-        public string? Species { get; set; }
+        [MaxLength(15, ErrorMessage = "Species has a max lenght of 15")] 
+        public string Species { get; set; }
 
-        [MaxLength(15)] 
+        [MaxLength(15, ErrorMessage = "Breed has a max lenght of 15")] 
         public string? Breed { get; set; }
         public string? DateOfBirth { get; set; }
-        [MaxLength(15)] 
+        [MaxLength(15, ErrorMessage = "Color has a max lenght of 15")] 
         public string? Color { get; set; }
         public bool? IsMicrochip { get; set; }
         public bool? IsNeutered { get; set; }
