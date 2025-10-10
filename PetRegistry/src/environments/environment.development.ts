cf2ed5ad86a5b269ae75daa0dbfcf5aa3f097811
blockplocker <noahallklint@gmail.com>
2025-10-10 10:17:51 +0200

    Update API_URL in development environment to match backend configuration

--- PetRegistry/src/environments/environment.development.ts
+++ PetRegistry/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 //For API url when working with development
 
 export const environment = {
-  API_URL: 'https://localhost:7046',
+  API_URL: 'https://localhost:44391',
   useLocalStorage: false, // Set to true to use local storage instead of API
 };
